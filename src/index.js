b3f030c427ce887373ea0faf90d0d7696547c3a1
deniskarandashev <39792231+deniskarandashev@users.noreply.github.com>
2021-08-22 00:59:39 +0300

    decision

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    let rev_num = 0;
+    if (n < 0) {
+        n = -1 * n;
+    }
+    while (n > 0)
+    {
+        rev_num = rev_num*10 + n%10;
+        n = Math.floor(n/10);
+    }
+    return rev_num;
 }
